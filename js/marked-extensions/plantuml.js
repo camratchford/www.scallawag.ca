e902baa73cbbed285803c9401117eac82c5cd2cf
camratchford <camratchford@gmail.com>
2025-02-06 01:16:07 +0000

    Refactored plantuml.js to account for changes in service api

--- js/marked-extensions/plantuml.js
+++ js/marked-extensions/plantuml.js
@@ -1,21 +1,53 @@
 
-async function encodeUML(uml_text) {
-    let data;
-    try {
-        await instance.request({
-            url: "plantuml-encoder",
-            method: "POST",
-            data: { uml: uml_text, uri: "", png: "" },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            data = response.data;
-        })
-    } catch (error) {
-        console.error('Error:', error);
-    }
-    return data;
+// async function encodeUML(uml_text) {
+//     let data;
+//     try {
+//         await instance.request({
+//             url: "plantuml-encoder",
+//             method: "POST",
+//             data: { uml: uml_text, uri: "", png: "" },
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         }).then(response => {
+//             data = response.data;
+//         })
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+//     return data;
+// }
+
+async function EncodeUML(placeholderId, pumlData, fileName) {
+    await instance.request({
+        url: "plantuml",
+        method: "POST",
+        data: { uml: pumlData, file_name: fileName },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        console.log(response.data)
+        let imgElement = document.getElementById(placeholderId);
+        imgElement.parentElement.href = response.data.url;
+        imgElement.innerHTML = response.data.svg;
+        imgElement.children[0].style = ""
+    })
+}
+
+async function loadUMLFile(fileName) {
+    let pumlData = "";
+    await instance.request({
+        url:  `content/${fileName}.puml`,
+        method: "GET",
+        data: {},
+        headers: {}
+    }).then(function (response) {
+        if (response.data) {
+            pumlData = response.data;
+        }
+    });
+    return pumlData;
 }
 
 
@@ -23,38 +55,78 @@ const plantuml = {
     name: 'plantuml',
     level: 'block',
     start(src) {
-        return src.startsWith("@startuml");
+        return src.indexOf('{plantuml:');
     },
     tokenizer(src, tokens) {
-        const rule = /^((@startuml)(.*?)(@enduml))/s;
+        const rule = /^\{plantuml:(.*?),(.*?)}/;
         const match = rule.exec(src);
 
         if (match) {
-            let token = {
+            return {
                 type: 'plantuml',
                 raw: match[0],
-                uml: match[3],
-                png: '',
+                fileName: match[1],
+                altText: match[2],
                 tokens: [],
             };
-            return token;
         }
     },
     renderer(token) {
-        let placeholderId = `uml-${Math.random().toString(36).substr(2, 9)}`;
-        encodeUML(token.uml).then(result => {
-            let source = `https://www.scallawag.ca/plantuml/${result.png}`
-            let img = document.getElementById(placeholderId)
-            img.src = source;
-            img.parentElement.href = source;
+        let placeholderId = `uml-${Math.random().toString(36).slice(2, 9)}`;
+        let fileName = token.fileName;
+        loadUMLFile(fileName).then((response) => {
+            EncodeUML(placeholderId, response, fileName).then((response) => {
 
-        });
+            })
+        })
+
+
         return `
         <a href="">
-            <img src="" id="${placeholderId}" class="uml">
+            <div id="${placeholderId}" class="uml"></div>    
         </a>
-        `
+`
     },
 };
+
+
+
+// const plantuml = {
+//     name: 'plantuml',
+//     level: 'block',
+//     start(src) {
+//         return src.startsWith("@startuml");
+//     },
+//     tokenizer(src, tokens) {
+//         const rule = /^((@startuml)(.*?)(@enduml))/s;
+//         const match = rule.exec(src);
+//
+//         if (match) {
+//             let token = {
+//                 type: 'plantuml',
+//                 raw: match[0],
+//                 uml: match[3],
+//                 png: '',
+//                 tokens: [],
+//             };
+//             return token;
+//         }
+//     },
+//     renderer(token) {
+//         let placeholderId = `uml-${Math.random().toString(36).substr(2, 9)}`;
+//         encodeUML(token.uml).then(result => {
+//             let source = `https://www.scallawag.ca/plantuml/${result.png}`
+//             let img = document.getElementById(placeholderId)
+//             img.src = source;
+//             img.parentElement.href = source;
+//
+//         });
+//         return `
+//         <a href="">
+//             <img src="" id="${placeholderId}" class="uml">
+//         </a>
+//         `
+//     },
+// };
 
 marked.use({extensions: [plantuml]});
