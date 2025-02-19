d99df2780d811c424776f96023d7165eaee5adc8
camratchford <camratchford@gmail.com>
2025-02-19 19:03:12 +0000

    Bugfix for page no automatically loading about.md when it's your first time

--- js/content.js
+++ js/content.js
@@ -5,8 +5,12 @@ function initPage() {
     let currentPage = localStorage.getItem("currentPage");
     if (currentPage && ! (pageRequested.indexOf("?md=") !== -1)) {
         renderContent(currentPage);
+    } else  if (! currentPage && ! (pageRequested.indexOf("?md=") !== -1)) {
+        localStorage.setItem("currentPage", "about.md");
+        renderContent("about.md");
     } else {
         navigateFromURL(document.location.toString());
     }
+
 }
 
